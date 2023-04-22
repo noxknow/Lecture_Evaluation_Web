b481a4b5ebedc24d76077ef3d0a4303b4c1828cd
noxknow <chris2769@naver.com>
2023-04-22 16:35:16 +0900

    first commit

---
+++ main/java/likey/LikeyDAO.java
@@ -0,0 +1,34 @@
+package likey;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import util.DatabaseUtil;
+
+public class LikeyDAO {
+	
+	public int like(String userID, String evaluationID, String userIP) {
+		String SQL = "INSERT INTO likey VALUES (?, ?, ?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			pstmt.setString(2, evaluationID);
+			pstmt.setString(3, userIP);
+			return pstmt.executeUpdate(); // 성공적이라면 1을 반환한다.
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try { if(conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if(pstmt != null) pstmt.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if(rs != null) rs.close(); } catch (Exception e) { e.printStackTrace(); }
+		}
+		return -1; // 추천 중복 오류
+	}
+}
